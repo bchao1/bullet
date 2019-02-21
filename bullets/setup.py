11f921823c5eacf4fc94995836c7293df6674457
Mckinsey666 <whereisbrianchao520@gmail.com>
2019-02-21 23:54:16 +0800

    bundle

---
+++ bullets/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='bullets',
+      version='0.1',
+      description='A robust Python list prompt tool.',
+      url='https://github.com/Mckinsey666/bullets',
+      author='Mckinsey666',
+      license='MIT',
+      packages=find_packages(),
+      )
