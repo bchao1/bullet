cfa3d8847be9ee4a57803ab152d546709201b83d
Mckinsey666 <whereisbrianchao520@gmail.com>
2019-02-22 19:51:28 +0800

    modify setup

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup, find_packages
 
-setup(name='bullets',
+setup(
+      name='bullets',
       version='0.1',
-      description='A robust Python list prompt tool.',
+      description='Beautiful Python list prompts made easy.',
+      long_description="Extensive support for Python list prompts \
+            formatting and colors",
       url='https://github.com/Mckinsey666/bullets',
+      keywords = "cli list prompt customize colors",
       author='Mckinsey666',
       license='MIT',
+      install_requires=[
+            'sys',
+            'os',
+            'termios',
+            'tty'
+      ]
       packages=find_packages(),
       )
