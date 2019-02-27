dc2671e8fffdbf6268e0fbdc73484f632bd4e266
Mckinsey666 <whereisbrianchao520@gmail.com>
2019-02-27 11:24:48 +0800

    change prompt example

--- examples/prompt.py
+++ examples/prompt.py
@@ -1,12 +1,13 @@
-from bullet import Bullet, Prompt, Check, Input, YesNo
+from bullet import Bullet, Prompt, Check, Input, YesNo, Numbers
 from bullet import styles
 
 cli = Prompt(
     [
-        Bullet("Choose from a list: ", **styles.Example),
-        Check("Choose from a list: ", **styles.Example),
+        YesNo("Are you a student? "),
         Input("Who are you? "),
-        YesNo("Are you a student? ")
+        Numbers("How old are you? "),
+        Bullet("What is your favorite programming language? ",
+              choices = ["C++", "Python", "Javascript", "Not here!"]),
     ],
     spacing = 2
 )
