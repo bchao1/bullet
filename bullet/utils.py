8950d0459169a544e89821d17dbacde44e5380ae
Antoine Delia <antoine.delia@technofy.io>
2019-10-24 16:28:29 +0200

    Fix Windows issues

--- bullet/utils.py
+++ bullet/utils.py
@@ -1,6 +1,7 @@
 import shutil
 import string
 import sys
+
 from . import colors
 
 COLUMNS, _ = shutil.get_terminal_size()  ## Size of console
@@ -103,51 +104,62 @@ def moveCursorLeft(n):
     ''' Move cursor left n columns. '''
     forceWrite("\033[{}D".format(n))
 
+
 def moveCursorRight(n):
     ''' Move cursor right n columns. '''
     forceWrite("\033[{}C".format(n))
-    
+
+
 def moveCursorUp(n):
     ''' Move cursor up n rows. '''
     forceWrite("\033[{}A".format(n))
+
 
 def moveCursorDown(n):
     ''' Move cursor down n rows. '''
     forceWrite("\033[{}B".format(n))
 
+
 def moveCursorHead():
     ''' Move cursor to the start of line. '''
-    forceWrite("\r")
+    # if the line is full, \r on Windows moves cursor to
+    # the next line, so we print \b first
+    forceWrite("\b\r")
+
 
 def clearLine():
     ''' Clear content of one line on the console. '''
-    forceWrite(" " * COLUMNS)
+    forceWrite(" " * COLUMNS + '\b')
     moveCursorHead()
-    
+
+
 def clearConsoleUp(n):
-    ''' Clear n console rows (bottom up). ''' 
+    ''' Clear n console rows (bottom up). '''
     for _ in range(n):
         clearLine()
         moveCursorUp(1)
 
+
 def clearConsoleDown(n):
-    ''' Clear n console rows (top down). ''' 
+    ''' Clear n console rows (top down). '''
     for _ in range(n):
         clearLine()
         moveCursorDown(1)
     moveCursorUp(n)
 
-def forceWrite(s, end = ''):
+
+def forceWrite(s, end=''):
     ''' Dump everthing in the buffer to the console. '''
     sys.stdout.write(s + end)
     sys.stdout.flush()
 
+
 def cprint(
-        s: str, 
-        color: str = colors.foreground["default"], 
-        on: str = colors.background["default"], 
+        s: str,
+        color: str = colors.foreground["default"],
+        on: str = colors.background["default"],
         end: str = '\n'
-    ):
+):
     ''' Colored print function.
     Args:
         s: The string to be printed.
@@ -157,4 +169,5 @@ def cprint(
     Returns:
         None
     '''
-    forceWrite(on + color + s + colors.RESET, end = end)
+
+    forceWrite(on + color + s + colors.RESET, end=end)
