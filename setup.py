d9830f3f467183a2e97eae346d396ac8b7ae92f4
Andrew Sheridan <sheridan@berkeley.edu>
2019-11-30 17:45:46 -0500

    Changed: Autoformatted `setup.py` with `black -l 120`

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-      name='bullet',
-      version='2.0.0',
-      description='Beautiful Python prompts made simple.',
-      long_description="Extensive support for Python list prompts \
-            formatting and colors",
-      url='https://github.com/Mckinsey666/bullets',
-      keywords = "cli list prompt customize colors",
-      author='Mckinsey666',
-      license='MIT',
-      packages=find_packages(),
-      )
+    name="bullet",
+    version="2.0.0",
+    description="Beautiful Python prompts made simple.",
+    long_description="Extensive support for Python list prompts formatting and colors",
+    url="https://github.com/Mckinsey666/bullets",
+    keywords="cli list prompt customize colors",
+    author="Mckinsey666",
+    license="MIT",
+    packages=find_packages(),
+)
