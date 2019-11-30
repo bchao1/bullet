c18fbe3846753d09f5fd2d0fc8fc3035cd3155cb
Andrew Sheridan <sheridan@berkeley.edu>
2019-11-30 17:57:55 -0500

    Changed: Autoformatted `examples/star.py` with `black -l 120`

--- examples/star.py
+++ examples/star.py
@@ -2,19 +2,19 @@ from bullet import Bullet
 from bullet import colors
 
 cli = Bullet(
-        prompt = "\nPlease choose a fruit: ",
-        choices = ["apple", "banana", "orange", "watermelon", "strawberry"], 
-        indent = 0,
-        align = 5, 
-        margin = 2,
-        bullet = "★",
-        bullet_color=colors.bright(colors.foreground["cyan"]),
-        word_color=colors.bright(colors.foreground["yellow"]),
-        word_on_switch=colors.bright(colors.foreground["yellow"]),
-        background_color=colors.background["black"],
-        background_on_switch=colors.background["black"],
-        pad_right = 5
-    )
+    prompt="\nPlease choose a fruit: ",
+    choices=["apple", "banana", "orange", "watermelon", "strawberry"],
+    indent=0,
+    align=5,
+    margin=2,
+    bullet="★",
+    bullet_color=colors.bright(colors.foreground["cyan"]),
+    word_color=colors.bright(colors.foreground["yellow"]),
+    word_on_switch=colors.bright(colors.foreground["yellow"]),
+    background_color=colors.background["black"],
+    background_on_switch=colors.background["black"],
+    pad_right=5,
+)
 
 result = cli.launch()
 print("You chose:", result)
