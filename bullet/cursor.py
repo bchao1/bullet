b78e8644401999a57dcc8493351040e4662b653f
Andrew Sheridan <sheridan@berkeley.edu>
2019-11-30 17:51:08 -0500

    Changed: Autoformatted `bullet/cursor.py` with `black -l 120`

--- bullet/cursor.py
+++ bullet/cursor.py
@@ -4,13 +4,13 @@ from contextlib import contextmanager
 
 # Show and hide cursors
 
-if os.name == 'nt':
+if os.name == "nt":
     import msvcrt
     import ctypes
 
     class _CursorInfo(ctypes.Structure):
-        _fields_ = [("size", ctypes.c_int),
-                    ("visible", ctypes.c_byte)]
+        _fields_ = [("size", ctypes.c_int), ("visible", ctypes.c_byte)]
+
 
 @contextmanager
 def hide():
@@ -20,24 +20,26 @@ def hide():
     finally:
         _show_cursor()
 
+
 def _hide_cursor():
-    if os.name == 'nt':
+    if os.name == "nt":
         ci = _CursorInfo()
         handle = ctypes.windll.kernel32.GetStdHandle(-11)
         ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
         ci.visible = False
         ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
-    elif os.name == 'posix':
+    elif os.name == "posix":
         sys.stdout.write("\033[?25l")
         sys.stdout.flush()
 
+
 def _show_cursor():
-    if os.name == 'nt':
+    if os.name == "nt":
         ci = _CursorInfo()
         handle = ctypes.windll.kernel32.GetStdHandle(-11)
         ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
         ci.visible = True
         ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
-    elif os.name == 'posix':
+    elif os.name == "posix":
         sys.stdout.write("\033[?25h")
         sys.stdout.flush()
