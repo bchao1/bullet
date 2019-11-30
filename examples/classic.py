e6d4f81043df2e451a39df1ed73404f2b87311e5
Andrew Sheridan <sheridan@berkeley.edu>
2019-11-30 17:55:24 -0500

    Changed: Autoformatted `examples/classic.py` with `black -l 120`

--- examples/classic.py
+++ examples/classic.py
@@ -1,15 +1,15 @@
 from bullet import Bullet
 
 cli = Bullet(
-        prompt = "\nPlease choose a fruit: ",
-        choices = ["apple", "banana", "orange", "watermelon", "strawberry"], 
-        indent = 0,
-        align = 5, 
-        margin = 2,
-        shift = 0,
-        bullet = "",
-        pad_right = 5
-    )
+    prompt="\nPlease choose a fruit: ",
+    choices=["apple", "banana", "orange", "watermelon", "strawberry"],
+    indent=0,
+    align=5,
+    margin=2,
+    shift=0,
+    bullet="",
+    pad_right=5,
+)
 
 result = cli.launch()
 print("You chose:", result)
