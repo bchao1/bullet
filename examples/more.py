8bcbe46805c39d24f503bc212b5e21bde4cca82c
Andrew Sheridan <sheridan@berkeley.edu>
2019-11-30 17:56:01 -0500

    Changed: Autoformatted `examples/more.py` with `black -l 120`

--- examples/more.py
+++ examples/more.py
@@ -2,11 +2,7 @@ from bullet import Bullet
 from bullet import Check
 from bullet import styles
 
-client = Check(
-    prompt = "Choose from a list: ",
-    **styles.Example,
-    **styles.Exam
-)
-print('\n', end = '')
+client = Check(prompt="Choose from a list: ", **styles.Example, **styles.Exam)
+print("\n", end="")
 result = client.launch()
 print(result)
