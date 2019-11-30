005afcaf68d2d842fbfbd5b1258834b479a711bd
Andrew Sheridan <sheridan@berkeley.edu>
2019-11-30 17:55:43 -0500

    Changed: Autoformatted `examples/colorful.py` with `black -l 120`

--- examples/colorful.py
+++ examples/colorful.py
@@ -2,20 +2,20 @@ from bullet import Bullet
 from bullet import colors
 
 cli = Bullet(
-        prompt = "\nPlease choose a fruit: ",
-        choices = ["apple", "banana", "orange", "watermelon", "strawberry"], 
-        indent = 0,
-        align = 5, 
-        margin = 2,
-        shift = 0,
-        bullet = "●",
-        bullet_color=colors.foreground["magenta"],
-        word_color=colors.foreground["red"],
-        word_on_switch=colors.foreground["green"],
-        background_color=colors.background["cyan"],
-        background_on_switch=colors.background["yellow"],
-        pad_right = 5
-    )
+    prompt="\nPlease choose a fruit: ",
+    choices=["apple", "banana", "orange", "watermelon", "strawberry"],
+    indent=0,
+    align=5,
+    margin=2,
+    shift=0,
+    bullet="●",
+    bullet_color=colors.foreground["magenta"],
+    word_color=colors.foreground["red"],
+    word_on_switch=colors.foreground["green"],
+    background_color=colors.background["cyan"],
+    background_on_switch=colors.background["yellow"],
+    pad_right=5,
+)
 
 result = cli.launch()
 print("You chose:", result)
