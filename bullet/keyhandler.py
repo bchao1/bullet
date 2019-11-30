00599bd58b0123cc18be4e72ea2fd7b088a085ad
Andrew Sheridan <sheridan@berkeley.edu>
2019-11-30 17:53:38 -0500

    Changed: Autoformatted `bullet/keyhandler.py` with `black -l 120`

--- bullet/keyhandler.py
+++ bullet/keyhandler.py
@@ -1,32 +1,37 @@
 from .charDef import UNDEFINED_KEY
 from . import utils
 
+
 def register(key):
-    '''
+    """
     Mark the function with the key code that it handles so
     that it can found by _KeyHandlerRegisterer.
-    '''
+    """
+
     def wrap(func):
-        if not hasattr(func, '_handle_key'):
-            setattr(func, '_handle_key', [key])
+        if not hasattr(func, "_handle_key"):
+            setattr(func, "_handle_key", [key])
         else:
             func._handle_key.append(key)
         return func
+
     return wrap
 
+
 def init(cls):
-    ''' Rewrite the class to include the _KeyHandlerRegisterer metaclass. '''
+    """ Rewrite the class to include the _KeyHandlerRegisterer metaclass. """
     return _KeyHandlerRegisterer(cls.__name__, cls.__bases__, cls.__dict__.copy())
+
 
 class _KeyHandlerRegisterer(type):
     def __new__(metacls, name, bases, classdict):
         result = super().__new__(metacls, name, bases, classdict)
-        if not hasattr(result, '_key_handler'):
-            setattr(result, '_key_handler', {})
-        setattr(result, 'handle_input', _KeyHandlerRegisterer.handle_input)
+        if not hasattr(result, "_key_handler"):
+            setattr(result, "_key_handler", {})
+        setattr(result, "handle_input", _KeyHandlerRegisterer.handle_input)
 
         for value in classdict.values():
-            handled_keys = getattr(value, '_handle_key', [])
+            handled_keys = getattr(value, "_handle_key", [])
             for key in handled_keys:
                 result._key_handler[key] = value
 
