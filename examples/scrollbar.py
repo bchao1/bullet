3235b7c3a46ffcdf41519eb3e8733900856ffe0b
Andrew Sheridan <sheridan@berkeley.edu>
2019-11-30 17:57:38 -0500

    Changed: Autoformatted `examples/scrollbar.py` with `black -l 120`

--- examples/scrollbar.py
+++ examples/scrollbar.py
@@ -3,18 +3,18 @@ from bullet import emojis
 from bullet import colors
 
 cli = ScrollBar(
-    "How are you feeling today? ", 
+    "How are you feeling today? ",
     emojis.feelings,
-    height = 5,
-    align = 5,
-    margin = 3,
-    pointer = "👉",
+    height=5,
+    align=5,
+    margin=3,
+    pointer="👉",
     pointer_color=colors.foreground["cyan"],
-    word_color = colors.foreground["white"],
+    word_color=colors.foreground["white"],
     word_on_switch=colors.foreground["white"],
     background_color=colors.background["black"],
-    background_on_switch=colors.background["black"]
+    background_on_switch=colors.background["black"],
 )
-print('\n')
+print("\n")
 result = cli.launch()
-print('\n')
+print("\n")
