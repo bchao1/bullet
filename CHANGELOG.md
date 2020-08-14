3a9cecf7746e417d8379e3cf138574ed75c49190
bchao1 <whereisbrianchao520@gmail.com>
2020-08-14 13:04:42 +0800

    update to 2.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 # Changelog
 
 All notable changes to this project will be documented in this file.
+
+## `bullet 2.2.0`
+- Add optional return indexes for `Bullet`, `Check`, and `Scrollbar`.
+- Fix `YesNo` keyhandler error (Issue #50) 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name='bullet',
-      version='2.0.0',
+      version='2.2.0',
       description='Beautiful Python prompts made simple.',
       long_description="Extensive support for Python list prompts \
             formatting and colors",
